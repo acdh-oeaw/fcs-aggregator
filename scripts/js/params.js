ad364829e335c8a0d46de0aa78508cea76b18b6e
Omar Siam <Omar.Siam@oeaw.ac.at>
2014-11-02 17:46:18 +0100

    Add a params.js that has the right relative paths when served by switch.php.

---
+++ scripts/js/params.js
@@ -0,0 +1,14 @@
+!function() {
+    var module = {};
+
+    module.baseURL = '../../';
+    module.userData = module.baseURL + '';
+    module.switchURL = module.baseURL + 'modules/fcs-aggregator/switch.php';
+    module.templateLocation = module.baseURL + 'scripts/js/';
+    module.defaultWidth = '3000px';
+    module.defaultHeight = '3000px';
+    module.defaultLeftOffset = -1000;
+    module.defaultTopOffset = -1000;
+
+    this.params = module;
+}();
